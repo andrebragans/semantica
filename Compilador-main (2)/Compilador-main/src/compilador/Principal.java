2a04e737fa87ec0c7250f98cbff668426e05529e
André Neves dos Santos Braga <38845628+andrebragans@users.noreply.github.com>
2024-11-28 08:11:21 -0300

    Delete Compilador-main (2)/Compilador-main directory

--- Compilador-main (2)/Compilador-main/src/compilador/Principal.java
+++
@@ -1,8 +0,0 @@
-package compilador;
-public class Principal {
-    public static void main(String[] args) {
-        Lexico lex = new Lexico(args[0]);
-        Sintatico sin = new Sintatico(lex);
-        sin.programa();
-    }
-}
--- Compilador-main (2)/Compilador-main/src/compilador/semantica/src/main/java/br/ufscar/parser3/EntradaTabelaDeSimbolos.java
+++
@@ -1,6 +0,0 @@
-package compilador.semantica.src.main.java.br.ufscar.parser3;
-
-public class EntradaTabelaDeSimbolos {
-    public String nome;
-    public double valor;
-}
